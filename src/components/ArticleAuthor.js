9db33710f58ddd4922af0446851206a496b57197
Eduard-90 <eddard1990@gmail.com>
2023-04-12 18:54:28 +0300

    finish lesson 38

---
+++ src/components/ArticleAuthor.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ArticleAuthor() {
+  return (
+    <p className="article__author">
+      <span>Author: Mike</span>
+      <span>Published: 06.12.2022</span>
+      <span>Theme: Video cards</span>
+    </p>
+  );
+}
+
+export default ArticleAuthor;
